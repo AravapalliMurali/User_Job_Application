6bd3d24b993c335ab403fc2d68ebfcbec95c3d43
murali krishna <muralikrishna@MacBook-Air.local>
2021-01-25 14:36:18 +0530

    adding job application react app and in that application form component is created

---
+++ jop_application/src/App.js
@@ -0,0 +1,12 @@
+import React from'react'
+import ApplicationForm from './ApplicationForm'
+
+export default function App(props){
+
+  return(
+    <div>
+      <h1>User Job Application </h1>
+      <ApplicationForm/>
+    </div>
+  )
+}
